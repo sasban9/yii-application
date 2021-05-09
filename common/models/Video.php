6dd4adc758e16815e56126f5788efdf16485aeb0
sasban <azure.saswata@gmail.com>
2021-05-09 21:31:03 +0530

    Create Video Table. Gii model Video

---
+++ common/models/Video.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%video}}".
+ *
+ * @property string $video_id
+ * @property string $title
+ * @property string|null $description
+ * @property string|null $tags
+ * @property int|null $status
+ * @property int|null $has_thumbnail
+ * @property string|null $video_name
+ * @property int|null $created_at
+ * @property int|null $updated_at
+ * @property int|null $created_by
+ */
+class Video extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%video}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['video_id', 'title'], 'required'],
+            [['description'], 'string'],
+            [['status', 'has_thumbnail', 'created_at', 'updated_at', 'created_by'], 'integer'],
+            [['video_id'], 'string', 'max' => 16],
+            [['title', 'tags', 'video_name'], 'string', 'max' => 512],
+            [['video_id'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'video_id' => Yii::t('app', 'Video ID'),
+            'title' => Yii::t('app', 'Title'),
+            'description' => Yii::t('app', 'Description'),
+            'tags' => Yii::t('app', 'Tags'),
+            'status' => Yii::t('app', 'Status'),
+            'has_thumbnail' => Yii::t('app', 'Has Thumbnail'),
+            'video_name' => Yii::t('app', 'Video Name'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+            'created_by' => Yii::t('app', 'Created By'),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\query\VideoQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\query\VideoQuery(get_called_class());
+    }
+}
